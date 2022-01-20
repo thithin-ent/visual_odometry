99da4b24d9b443a3d316d74a0269f94faf6c3a99
thithin <kks980821@gmail.com>
2022-01-20 21:48:40 +0900

    add README

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # visual_odometry
 
-visudal odometry using PnP solve
+visual odometry using PnP solve
 
 ## Dependencies
 * Eigen 3
 * RealSense2
 * OpenCV 3.4
+
+# how to use
+
+  1. git clone repository
+  2. cd visual_odometry/build
+  3. cmake ..
+  4. make
+  5. cd ../bin
+  6. ./(bin_file)
---
+++ src/CMakeFiles/pose_image_TUM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_image_TUM.dir/EKF.cpp.o"
+  "CMakeFiles/pose_image_TUM.dir/camera.cpp.o"
+  "CMakeFiles/pose_image_TUM.dir/pose_image_TUM.cpp.o"
+  "CMakeFiles/pose_image_TUM.dir/image.cpp.o"
+  "../bin/pose_image_TUM.pdb"
+  "../bin/pose_image_TUM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_image_TUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
